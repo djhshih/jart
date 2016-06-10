4ac91ac570c90a875eead0c5bced272ec366e5ed
David J. H. Shih <djh.shih@gmail.com>
2016-06-10 10:44:31 -0400

    Fixed JAR_PATH not defined error.

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ set -o errexit
 set -o nounset
 
 DESTDIR=${DESTDIR:-/usr/local}
+JART_PATH=${JAR_PATH:-$HOME/java}
 
 name="jartools"
 version="0.1"
@@ -11,4 +12,6 @@ version="0.1"
 basepath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 install $basepath/bin/* $DESTDIR/bin
+
+[[ ! -d $JART_PATH ]] && mkdir -p $JART_PATH
 
