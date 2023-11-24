b1aa47d26c4ec5869ecd4b99504c869c3dfc0595
David J. H. Shih <djh.shih@gmail.com>
2023-11-24 16:50:26 +0800

    Install scripts with harded paths.

--- install.sh
+++ install.sh
@@ -4,14 +4,22 @@ set -o errexit
 set -o nounset
 
 DESTDIR=${DESTDIR:-/usr/local}
-JART_PATH=${JAR_PATH:-$HOME/java}
+JAR_PATH=${JAR_PATH:-$DESTDIR/java}
 
 name="jartools"
 version="0.1"
 
 basepath="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-install $basepath/bin/* $DESTDIR/bin
+[[ ! -d $JAR_PATH ]] && mkdir -p $JAR_PATH
 
-[[ ! -d $JART_PATH ]] && mkdir -p $JART_PATH
+_install() {
+	fpath=$1
+	fname=${fpath##*/}
+	sed "s|JAR_PATH=.*|JAR_PATH=$JAR_PATH|" $fpath > $DESTDIR/bin/$fname
+}
+
+for f in $basepath/bin/*; do
+	_install $f
+done
 
